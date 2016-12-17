85a14723ecaee93b75d52aa73309e7c12444a404
klaus31 <danieloltmanns@gmx.de>
2016-12-17 06:59:24 +0100

    💩

--- src/test/java/javaformatter/decider/java/JavaDeciderForSpacesDefaultTest.java
+++ src/test/java/javaformatter/decider/java/JavaDeciderForSpacesDefaultTest.java
@@ -1,5 +1,6 @@
 package javaformatter.decider.java;
 
+import org.junit.Ignore;
 import org.junit.Test;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.*;
@@ -12,5 +13,14 @@ public class JavaDeciderForSpacesDefaultTest {
         assertThat(decider.modifySpacesInPartOfLine("public int calculatereturnCode() {"), is("public int calculatereturnCode() {"));
         assertThat(decider.modifySpacesInPartOfLine("int returnCode = 5;"), is("int returnCode = 5;"));
         assertThat(decider.modifySpacesInPartOfLine("return     returnCode;"), is("return returnCode;"));
+        assertThat(decider.modifySpacesInPartOfLine("List<String, Map<Object, String>>"), is("List<String, Map<Object, String>>"));
+    }
+
+    @Test
+    @Ignore // this 💩
+    public void ichGebeDieseGanzeScheisseAufUndFangeVonVorneAnWeilDieseScheisseHierNichtFunktioniertTest() {
+        JavaDeciderForSpacesDefault decider = new JavaDeciderForSpacesDefault();
+        assertThat(decider.modifySpacesInPartOfLine("List<String extends Foo> r, String s, List<?> h) { "), is("List<String> r, String s, List<File> h) { "));
+        assertThat(decider.modifySpacesInPartOfLine("List<Zoo super Off, ? extends Foo> r, String s, List<? super Bla> h) { "), is("List<String> r, String s, List<File> h) { "));
     }
 }
