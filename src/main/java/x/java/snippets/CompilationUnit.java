42f1789a23775e4a90419ad518ced6202faa1e88
klaus31 <danieloltmanns@gmx.de>
2017-01-04 11:51:58 +0100

    kill decorated java code snippet

--- src/main/java/x/java/snippets/CompilationUnit.java
+++ src/main/java/x/java/snippets/CompilationUnit.java
@@ -1,10 +1,51 @@
 package x.java.snippets;
 
+import x.format.CodeSnippet;
 import x.java.JavaRulePath;
+import x.java.NodeWrapper;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.function.Consumer;
+
+import static java.util.stream.Collectors.joining;
 import static x.java.JavaConfig.getMatchingCodeSnippetFor;
 
-public class CompilationUnit extends DecoratedJavaCodeSnippet {
+public class CompilationUnit implements JavaCodeSnippet {
+
+    private Optional<JavaCodeSnippet> currentCodeSnippet;
+    private List<JavaCodeSnippet> snippets;
+
+
+    public CompilationUnit() {
+        snippets = new ArrayList<>();
+        currentCodeSnippet = Optional.empty();
+    }
+
+    private void setCurrentCodeSnippet(JavaCodeSnippet javaCodeSnippet) {
+        flushCurrentSnippetIfPresent();
+        currentCodeSnippet = Optional.ofNullable(javaCodeSnippet);
+    }
+
+    private void flushCurrentSnippetIfPresent() {
+        withCurrentSnippetIfPresent(snippets::add);
+    }
+
+    private void withCurrentSnippetIfPresent(Consumer<JavaCodeSnippet> consumer) {
+        currentCodeSnippet.ifPresent(consumer);
+    }
+
+    @Override
+    public void add(NodeWrapper node) {
+        currentCodeSnippet.orElseThrow(() -> new AssertionError("Sorry for X-Formatter is unable to format valid but unexpected: " + node)).add(node);
+    }
+
+    @Override
+    public String toSourceString() {
+        flushCurrentSnippetIfPresent();
+        return snippets.stream().map(CodeSnippet::toSourceString).collect(joining());
+    }
 
     @Override
     public void enterRule(JavaRulePath rulePath) {
