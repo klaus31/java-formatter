5f28604ea2987d331861cd896e6790faa8f272a9
klaus31 <danieloltmanns@gmx.de>
2017-01-07 10:25:30 +0100

    Friendly Fire

--- src/main/java/x/java/snippets/MethodBody.java
+++ src/main/java/x/java/snippets/MethodBody.java
@@ -2,17 +2,15 @@ package x.java.snippets;
 
 import org.antlr.v4.runtime.tree.ParseTree;
 import x.java.NodeWrapper;
-
 import static java.util.Arrays.asList;
 import static x.java.JavaConfig.EOL;
 import static x.java.JavaConfig.getIndentService;
-
 public class MethodBody extends SimpleNodesJavaCodeSnippet {
     @Override
     protected String toSourceString(NodeWrapper node) {
         StringBuilder result = new StringBuilder();
         result.append(node.toSourceString());
-        if (!onlyPureNodeValueIsRequired(node)) {
+        if (! onlyPureNodeValueIsRequired(node)) {
             if (requiresSingleBlankAfter(node)) {
                 result.append(" ");
             } else if (isAEolCandidate(node)) {
@@ -22,16 +20,12 @@ public class MethodBody extends SimpleNodesJavaCodeSnippet {
         }
         return result.toString();
     }
-
     private boolean isAEolCandidate(NodeWrapper node) {
         return node.isBlockStartOrEnd() || node.isSemicolonAtEnd() || ":".equals(node.toSourceString());
     }
-
     private boolean onlyPureNodeValueIsRequired(NodeWrapper node) {
-        return node.isBlockEnd() && asList(")", ";").contains(node.calculateNext().getText())
-                || node.isBlockStart() && node.matchesRulePath("arrayInitializer");
+        return node.isBlockEnd() && asList(")", ";").contains(node.calculateNext().getText()) || node.isBlockStart() && node.matchesRulePath("arrayInitializer");
     }
-
     private boolean requiresSingleBlankAfter(NodeWrapper node) {
         if (isAEolCandidate(node)) {
             if (node.isNextNodeACommentInSameLine() || node.isBlockEnd() && node.isNextNodeElseCatchOrWhile()) {
@@ -58,10 +52,10 @@ public class MethodBody extends SimpleNodesJavaCodeSnippet {
         }
         if (nextNode.getText().equals(":")) {
             // TODO for each in case block and named statements
-            return !node.matchesRulePath("switchBlock");
+            return ! node.matchesRulePath("switchBlock");
         }
         if (")".equals(node.toSourceString())) {
-            return !asList(".").contains(nextNode.getText());
+            return ! asList(".").contains(nextNode.getText());
         }
         if (node.matchesRulePath("unannClassType_lfno_unannClassOrInterfaceType")) {
             if (asList("<", ",", ">").contains(nextNode.getText())) {
